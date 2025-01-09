773b63071f6f677f69d38e54fe0ed50104e70451
Shilpi Parashar <parashar.shilpi28@gmail.com>
2025-01-10 00:11:10 +0530

    deploy changes

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ Participant.get
 const app = express();
 app.use(
   cors({
-    origin: ["https://deploy-mern- .vercel.app"], // Allow these origins
+    origin: ["https://msff-project-frontend.vercel.app/"], // Allow these origins
     methods: ["GET", "POST"], // Allow specific HTTP methods
     credentials: true, // Allow cookies/authorization headers
   })
